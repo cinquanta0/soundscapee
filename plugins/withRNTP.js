bf376e9ade0e3463a209a640096759dbdceae87a
Luca <rosangela.calasso69@gmail.com>
2026-04-22 22:34:34 +0200

    fix(android): force Gradle to link react-native-track-player manually
    Since RN 0.81 CLI completely ignores RNTP 4.1.2 for autolinking, injecting it into MainApplication.kt caused a compilation error because Gradle had no idea where 'com.doublesymmetry' came from. This updates the Expo plugin to manually inject the Gradle project inclusion in settings.gradle and app/build.gradle.

--- plugins/withRNTP.js
+++ plugins/withRNTP.js
@@ -1,4 +1,4 @@
-const { withAndroidManifest, withMainApplication } = require('@expo/config-plugins');
+const { withAndroidManifest, withMainApplication, withSettingsGradle, withAppBuildGradle } = require('@expo/config-plugins');
 
 const withRNTPManifest = (config) => {
   return withAndroidManifest(config, (config) => {
@@ -62,9 +62,37 @@ const withRNTPMainApplication = (config) => {
   });
 };
 
+const withRNTPGradle = (config) => {
+  config = withSettingsGradle(config, (config) => {
+    const contents = config.modResults.contents;
+    if (!contents.includes("':react-native-track-player'")) {
+      config.modResults.contents = contents + `
+include ':react-native-track-player'
+project(':react-native-track-player').projectDir = new File(rootProject.projectDir, '../node_modules/react-native-track-player/android')
+`;
+    }
+    return config;
+  });
+
+  config = withAppBuildGradle(config, (config) => {
+    let contents = config.modResults.contents;
+    if (!contents.includes("project(':react-native-track-player')")) {
+      contents = contents.replace(
+        /dependencies\s*\{/,
+        "dependencies {\n    implementation project(':react-native-track-player')"
+      );
+      config.modResults.contents = contents;
+    }
+    return config;
+  });
+
+  return config;
+};
+
 const withRNTP = (config) => {
   config = withRNTPManifest(config);
   config = withRNTPMainApplication(config);
+  config = withRNTPGradle(config);
   return config;
 };
 
