077dcb6c7c8da57dbda1251016967da90b4ca1d2
Luca <rosangela.calasso69@gmail.com>
2026-05-04 13:11:39 +0200

    fix: avoid fetch-based local storage uploads

--- services/storageUpload.js
+++ services/storageUpload.js
@@ -1,3 +1,4 @@
+import { File } from 'expo-file-system';
 import * as FileSystem from 'expo-file-system/legacy';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { auth, storage } from '../firebaseConfig';
@@ -38,10 +39,9 @@ async function uploadViaRest(storagePath, localUri, contentType) {
 }
 
 async function uploadViaSdk(storagePath, localUri, contentType) {
-  const response = await fetch(localUri);
-  const blob = await response.blob();
   const storageRef = ref(storage, storagePath);
-  await uploadBytes(storageRef, blob, { contentType });
+  const fileBlob = new File(localUri);
+  await uploadBytes(storageRef, fileBlob, { contentType });
   return getDownloadURL(storageRef);
 }
 
